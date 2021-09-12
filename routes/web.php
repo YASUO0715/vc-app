16424928acc96c4e13af90125902cdc884885e95
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-12 13:52:54 +0900

    Language機能実装

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,7 @@ Route::get('/view/word/{name}/{msg}', function($name, $msg) {
  Route::get('/controller/var', [App\Http\Controllers\MessageController::class, 'var']);
  Route::get('/controller/word/{msg}', [App\Http\Controllers\MessageController::class, 'word']);
  Route::get('/controller/word/{name}/{msg}', [App\Http\Controllers\MessageController::class, 'word2']);
+
+Route::get('language', 
+[App\Http\Controllers\LanguageController::class, 'index']);
+

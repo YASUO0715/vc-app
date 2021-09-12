dcf9e6daadfbe33ee66b29334f5653a10cf46135
YASUO0715 <yasuo0715niihori@gmail.com>
2021-09-12 13:06:56 +0900

    VCApp完成

--- resources/views/message/hello.blade.php
+++ resources/views/message/hello.blade.php
@@ -8,3 +8,16 @@
      <h1>Hello from View!!</h1>
 </body>
 </html>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+     <meta charset="UTF-8">
+     <title>VCApp</title>
+     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+     
+</head>
+<body>
+    <h2>Hello View!!</h2>
+</body>
+</html>
